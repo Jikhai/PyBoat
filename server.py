f8afe6e616729878752ecc25e6f79157dec0b760
Jikhai <reldin.d@gmail.com>
2019-11-02 20:10:42 +0100

    Gestion des connexion
    Uniquement 2 joueurs connectés à la fois, avertissement pour les clients supplémentaires

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ clientlist = []
 #----                       ----#
 
 
-def SockGestion():
+def SockGestion(): # controls the opening and closing of sockets.
     try :
         lesocket = socket.socket(socket.AF_INET6,socket.SOCK_STREAM, 0)
     except Exception as err:
@@ -31,21 +31,41 @@ def SockGestion():
 
     lesocket.listen(1)
     usrcount = 0
-
+    # just a list of preset notifications to send to the clients
     notifforfeit = ("Your opponent left, or was disconnected, you win by default.\n").encode("utf_8")
     #greetingnick =("please pick a username using the command : NICK <username> you can get a list of commands with HELP\n").encode("utf_8")
-    greeting =("Welcome to BattleShip !").encode("utf_8")
-    while True : #tests avec nc localhost 7777
+    greeting =("Welcome to BattleShip !\n").encode("utf_8")
+    warning =("The server is already handling a game between two players\n, closing connection now.\n").encode("utf_8")
+    
+
+    #these will be the sockets for the two players
+    player1 = ''
+    player2 = ''
+
+    while True : #tests avec nc localhost 7777 > will do a local client later on
 
         socklist,list_a,list_b = select.select(clientlist + [lesocket],[],[])    
         for i in socklist : 
             if i == lesocket :
                 established, addr = lesocket.accept()
                 clientlist.append(established)
+                if player1 == '' : # here we're going to check if the two players
+                    player1 = established # exist, and refuse further connections
+                    player1.send(greeting)
+                elif player2 == '' :
+                    player2 = established
+                    player2.send(greeting)
+                else :
+                    established.send(greeting)
+                    established.send(warning)
+                    established.close()
+                    clientlist.remove(established)
+                    usrcount-=1 #because it would break the count otherwise
                 usrcount +=1 
                 print("one more user connected, total : ",usrcount) 
-                established.send(greeting)      
-            else :
+                # print(clientlist) debug purposes
+               
+            else : # checking if the sockets are being closed, and what the clients are sending.
                 text=i.recv(1500)
                 if len(text) == 0 :
                     i.close()
