eb327abdcf0de1d6419c1ea335bf96ca0ef0cf9e
léna <alylena.hadjlazib@gmail.com>
2019-11-04 21:39:52 +0100

    test avec displayGame, mis en commentaire

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from game import *
+from main import *
+from server import *
 import os
 import sys
 import string
 import socket
 def ClieGestion():
 
-    #TODO establish connection to the server : requires having a useable address for the server. 
+    #TODO establish connection to the server : requires having a useable address for the server.
     try :
         lesocket = socket.socket(socket.AF_INET6,socket.SOCK_STREAM, 0)
     except Exception as err:
@@ -18,11 +20,12 @@ def ClieGestion():
     except Exception as err :
         print("Failure --> ",err)
         sys.exit(-1)
-    
+
     data = lesocket.recv(2048).decode("UTF_8")
     print(data)
     #TODO : Contrôl the socket properly -> send, AND recieve data without breaking stuff, current code can establish a connection but this is not enough.
 
+    #displayGame(game, data) #the display is related to the recieved data
     #this should be handled by the server.
     #print("initializing game, setting up boats")
     #boats = randomConfiguration()
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+from game import *
+from main import *
 import time
-import os 
+import os
 import sys
 import string
 import select
@@ -41,29 +42,33 @@ def SockGestion(): # controls the opening and closing of sockets.
     #greetingnick =("please pick a username using the command : NICK <username> you can get a list of commands with HELP\n").encode("utf_8")
     greeting =("Welcome to BattleShip !\n").encode("utf_8")
     warning =("The server is already handling a game between two players\n, closing connection now.\n").encode("utf_8")
-    
 
+    #the initialisation of the game
+    #boats1 = randomConfiguration()
+    #boats2 = randomConfiguration()
+    #game = Game(boats1, boats2)
     #these will be the sockets for the two players
     player1 = ''
     player2 = ''
 
     while True : #tests avec nc localhost 7777 > will do a local client later on
 
-        socklist,list_a,list_b = select.select(clientlist + [lesocket],[],[])    
-        for i in socklist : 
+        socklist,list_a,list_b = select.select(clientlist + [lesocket],[],[])
+        for i in socklist :
             if i == lesocket :
                 established, addr = lesocket.accept()
                 clientlist.append(established)
                 if player1 == '' : # here we're going to check if the two players
                     player1 = established # exist, and refuse further connections
                     player1.send(greeting)
-                    player1.send(("you are player 1\n").encode("UTF_8"))
+                    player1.send(("you are player 1\n")
+                    #player1.send("With ID=0\nEnter your ID : \n").encode("UTF_8"))
                     print("connection to player 1 established !")
-
                 elif player2 == '' :
                     player2 = established
                     player2.send(greeting)
-                    player2.send(("you are player 2\n").encode("UTF_8"))
+                    player2.send(("you are player 2\n")
+                    #player2.send("With ID=1\nEnter your ID : \n").encode("UTF_8"))
                     print("connection to player 2 established !")
                 else :
                     established.send(greeting)
@@ -71,10 +76,10 @@ def SockGestion(): # controls the opening and closing of sockets.
                     established.close()
                     clientlist.remove(established)
                     usrcount-=1 #because it would break the count otherwise
-                usrcount +=1 
-                print("one more user connected, total : ",usrcount,"\n") 
+                usrcount +=1
+                print("one more user connected, total : ",usrcount,"\n")
                 # print(clientlist) debug purposes
-               
+
             else : # checking if the sockets are being closed, and what the clients are sending.
                 text=i.recv(1500)
                 if len(text) == 0 :
@@ -91,8 +96,7 @@ def SockGestion(): # controls the opening and closing of sockets.
                 else :
                     #that's where the magic will happen
                     print ("data transmitted from :", i,"\n")
-                    
-    else : 
+
+    else :
         lesocket.close()
         sys.exit(0)
-
