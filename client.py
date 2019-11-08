31fd263e349e1243dd91b28d5741a1884332babd
léna <alylena.hadjlazib@gmail.com>
2019-11-08 22:31:47 +0100

    stock dans une liste -> erreur!

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from main import *
-from game import *
+#from game import *
 from server import *
 import os
 import sys
@@ -23,21 +23,37 @@ def ClieGestion():
 
     data = lesocket.recv(1500).decode("UTF_8")
     print(data)
+
     #TODO à déclarer les variables de mémoire de jeu
     #reception des placements de bateau et stockage dans un tableau
-    b = Boat()
+    #b = Boat()
     boats = []
-    print("\n")
-    for i in range(5):
+    print("--------------------------------------------------------\n")
+    for i in range(10):
         x = lesocket.recv(1024)
-        b.x = x
+        boats[i] = x
         #Test print
-        print(x)
         y = lesocket.recv(1024)
-        b.y = y
+        boats[i + 1] = y
         #Test print
-        print(y)
-        boats.append(b)
+        i = i + 1
+
+    for i in range(10):
+        if i%2 == 0:
+            print("x = "+ boats[i])
+        else:
+            print("y = "+ boats[i])
+        print("\n")
+    #for i in range(5):
+    #    x = lesocket.recv(1024)
+    #    b.x = x
+    #    #Test print
+    #    print(x)
+    #    y = lesocket.recv(1024)
+    #    b.y = y
+    #    #Test print
+    #    print(y)
+    #    boats[i] = b
 
     while True :
         a=0
--- server.py
+++ server.py
@@ -61,11 +61,7 @@ def SockGestion(): # controls the opening and closing of sockets.
                 if player1 == '' : # here we're going to check if the two players
                     player1 = established # exist, and refuse further connections
                     player1.send(greeting)
-                    player1.send(("you are player 1").encode("UTF_8"))
-                    #sending the boats1 position (x,y)
-                    for b1 in boats1:
-                        player1.send(b"%d"%b1.x)
-                        player1.send(b"%d"%b1.y)
+                    player1.send(("you are player 1\n").encode("UTF_8"))
                     #while True :
                     #    player1.send(("woof").encode("UTF_8"))
                     #player1.send(("With ID=0\nEnter your ID : \n").encode("UTF_8"))
@@ -73,11 +69,7 @@ def SockGestion(): # controls the opening and closing of sockets.
                 elif player2 == '' :
                     player2 = established
                     player2.send(greeting)
-                    player2.send(("you are player 2").encode("UTF_8"))
-                    #sending the boats2 position (x,y)
-                    for b2 in boats2:
-                        player2.send(b"%d"%b2.x)
-                        player2.send(b"%d"%b2.y)
+                    player2.send(("you are player 2\n").encode("UTF_8"))
                     #player2.send(("With ID=1\nEnter your ID : \n").encode("UTF_8"))
                     print("connection to player 2 established !")
                 else :
@@ -89,6 +81,14 @@ def SockGestion(): # controls the opening and closing of sockets.
                 usrcount +=1
                 print("one more user connected, total : ",usrcount,"\n")
                 # print(clientlist) debug purposes
+                #sending the boats1 position (x,y)
+                for b1 in boats1:
+                    player1.send(b"%d"%b1.x)
+                    player1.send(b"%d"%b1.y)
+                #sending the boats2 position (x,y)
+                for b2 in boats2:
+                    player2.send(b"%d"%b2.x)
+                    player2.send(b"%d"%b2.y)
 
             else : # checking if the sockets are being closed, and what the clients are sending.
                 text=i.recv(1500)
