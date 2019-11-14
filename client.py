ca19f1bf6fa68fc01e000de46f8e6718f36ed343
Alyssa Lena Hadj Lazib <ahadjlazib@emi.u-bordeaux1.fr>
2019-11-14 13:19:52 +0100

    Display function

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from game import *
-from main import *
+import main
 from server import *
 import pickle
 import os
@@ -8,6 +8,7 @@ import sys
 import string
 import socket
 def ClieGestion(): #all of the logic for the client side
+    shots = []
     boats = ''
     try :
         lesocket = socket.socket(socket.AF_INET6,socket.SOCK_STREAM, 0)
@@ -20,18 +21,19 @@ def ClieGestion(): #all of the logic for the client side
     except Exception as err :
         print("Failure --> ",err)
         sys.exit(-1)
-    
+
     while True :
         data = lesocket.recv(4096)
         if len(data) ==0 :
             print("connection to server lost !")
             lesocket.close()
         else :
-            try : 
-                data = data.decode("UTF_8") 
+            try :
+                data = data.decode("UTF_8")
             except Exception as err :
                 #print("not a regular message :p") #debug
                 data = pickle.loads(data)
+                Display(data, data, shots, shots)
             print(data)
             print("--------------------------------------------------------\n")
 
@@ -39,7 +41,14 @@ def ClieGestion(): #all of the logic for the client side
     #reception des placements de bateau et stockage dans un tableau
     #b = Boat()
     #boats = []
-   
+
+
+
+def Display(boats1, boats2, shots1, shots2):
+    main.displayConfiguration(boats1, shots1, True)
+    main.displayConfiguration(boats2, shots2, False)
+
+
 
     ''' la logique grosso merdo
         affichage()
